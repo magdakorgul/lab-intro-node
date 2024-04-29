a935cca3ea00f7f5c8723cfd32812f8f40060f04
magdakorgul <magdalenakorgul1@gmail.com>
2024-04-29 20:41:00 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    } else {
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("Empty SortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
-
-  avg() {}
+  sum() {
+    let total = 0;
+    if (this.items.length === null) {
+      return 0;
+    } else {
+      this.items.forEach(function (num) {
+        total += num;
+      });
+    }
+    return total;
+  }
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("Empty SortedList");
+    } else {
+      let total = this.total();
+    }
+    return total / this.items.length;
+  }
 }
 
 module.exports = SortedList;
